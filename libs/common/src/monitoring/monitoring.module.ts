aa3ddfb4d880d4348047a9a029897ac17196eeb1
okjodom <okjodom@gmail.com>
2025-04-03 14:51:57 +0300

    feat(monitoring): update MonitoringModule to support dynamic registration of metrics services

--- libs/common/src/monitoring/monitoring.module.ts
+++ libs/common/src/monitoring/monitoring.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { LnurlMetricsService } from './lnurl-metrics.service';
+import { OperationMetricsService } from './metrics.service';
 
+/**
+ * Base monitoring module with core metrics services
+ */
 @Module({
   imports: [EventEmitterModule.forRoot()],
   providers: [LnurlMetricsService],
   exports: [LnurlMetricsService],
 })
-export class MonitoringModule {}
+export class MonitoringModule {
+  /**
+   * Register additional custom metric services
+   * @param metricServices Array of metric service providers to register
+   * @returns Dynamic module with custom metric services
+   */
+  static forFeature(metricServices: any[] = []): DynamicModule {
+    return {
+      module: MonitoringModule,
+      providers: [...metricServices],
+      exports: [...metricServices],
+    };
+  }
+}
